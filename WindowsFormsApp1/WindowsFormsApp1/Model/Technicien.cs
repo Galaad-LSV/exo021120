2ac4f1ef58d76a41b5c617943d4ee61508a753b4
Galaad LUSVEN <galaad.lusven@hotmail.com>
2020-11-02 11:12:19 +0100

    Modification Technicien

--- WindowsFormsApp1/WindowsFormsApp1/Model/Technicien.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Model/Technicien.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 
-public class Class1
+public class Technicien
 {
+    #region attributs
     public static List<Technicien> CollClassStation = new List<Technicien>();
-    private int id;
-    private string nom;
-    private string prenom;
-    public List<Tournee>;
+    private int _id;
+    private string _nom;
+    private string _prenom;
+    public List<Tournee> lesTournees;
+    #endregion
+
+    #region constructeurs
+    public Technicien(int id, string nom, string prenom)
+    {
+        this._id = id;
+        this._nom = nom;
+        this._prenom = prenom;
+        CollClassStation.Add(this);
+    }
+    #endregion
+
+    #region getters/setters
+
+    #endregion
+
+    #region methodes
+    #endregion
+
+
 }
